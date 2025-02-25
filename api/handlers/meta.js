6e95d8b58fdce6e8e2e7ce5bbb779f30b0302a8c
tommirp <tommirp@gmail.com>
2025-02-25 10:37:36 +0700

    meta

--- api/handlers/meta.js
+++ api/handlers/meta.js
@@ -2,7 +2,38 @@ const url = require('../../data/meta.json');
 
 class MetaHandler {
   static GetYoutubeLink(_, res) {
-    return res.status(200).send(url);
+    const meta = [
+      {
+        "number": 1,
+        "metaName": "Youtube Mekah Primary",
+        "metaType": "youtube",
+        "metaValue": url.youtubeMekahPrimary,
+        "metaDescription": "Youtube Mekah Primary"
+      },
+      {
+        "number": 2,
+        "metaName": "Youtube Mekah Secondary",
+        "metaType": "youtube",
+        "metaValue": url.youtubeMekahSecondary,
+        "metaDescription": "Youtube Mekah Secondary"
+      },
+      {
+        "number": 3,
+        "metaName": "Youtube Madinah Primary",
+        "metaType": "youtube",
+        "metaValue": url.youtubeMadinahPrimary,
+        "metaDescription": "Youtube Madinah Primary"
+      },
+      {
+        "number": 4,
+        "metaName": "Youtube Madinah Secondary",
+        "metaType": "youtube",
+        "metaValue": url.youtubeMadinahSecondary,
+        "metaDescription": "Youtube Madinah Secondary"
+      }
+    ];
+
+    return res.status(200).send(meta);
   }
 }
 
